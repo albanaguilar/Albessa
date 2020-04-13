1848f4dc64cd33231fbcea50b252e81a0cae2925
albanaguilar <albanaguilar1@gmail.com>
2020-04-13 00:48:23 -0500

    Parser done, working on Object Generation

---
+++ Objects/varObject.py
@@ -0,0 +1,7 @@
+
+class variableObject(object):
+	"""docstring for variableObject"""
+	def __init__(self):
+
+		#this will hold the python exec string for the variable declaracion
+		self.exec_string = ""
