1cf25097f29b6486b1bf2a188be252fa41b6e9e4
albanaguilar <albanaguilar1@gmail.com>
2020-05-14 23:31:36 -0500

    Implement functions, working on cubo semantico

--- ply-master/cuboSemantico.py
+++ ply-master/cuboSemantico.py
@@ -3,8 +3,6 @@ import sys
 #cubo semantico sirve para cuando se hacen operaciones aritmeticas, logicas, comparaciones, asignaciones
 
 class cuboSemantico():
-
-	#funciones 
 	
 	def __init__(self, arg):
 		super(cuboSemantico, self).__init__()
